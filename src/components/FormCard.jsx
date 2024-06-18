1d030d85db94b0a514a5737153ac309bdbc89a2c
paussaanz <paula@sanz.eu>
2024-06-18 18:26:47 +0200

    form card h-100 misma altura

--- src/components/FormCard.jsx
+++ src/components/FormCard.jsx
@@ -3,12 +3,12 @@ import Button from './Button';
 
 const FormCard = ({ imageSrc, imageAlt, title, text, imageClass, buttonText, buttonHref }) => {
     return (
-        <div className="card">
+        <div className="card h-100">
             <img src={imageSrc} className={`card-img-top ${imageClass}`} alt={imageAlt} />
             <div className="card-body">
+                <p className="legend text-primary">nº de views</p>
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{text}</p>
-                <Button text={buttonText} variant="primary" href={buttonHref} />
             </div>
         </div>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,70 +4,70 @@ const HomePage = () => {
 
 
 
-  return (
-    <>
-      <section className="forma-corp container">
-            <h1 className="text-center">Descubre todas las formaciones disponibles sobre tu empresa o mercado</h1>
-            <div className="row pb-5">
-                <div className="col-12 pb-5">
-                    <FormCard
-                        imageSrc="./src/assets/dist/img/klem-logo.png"
-                        imageAlt="Klem Logo"
-                        title="Card Title 2"
-                        text="Some quick example text to build on the card title and make up the bulk of the card's content."
-                        imageClass="aspect-formacorp"
-                        buttonText="Go somewhere"
-                        buttonHref="/form-detail"
-                    />
-                </div>
-                <div className="col-3">
-                    <FormCard
-                        imageSrc="./src/assets/dist/img/klem-logo.png"
-                        imageAlt="Klem Logo"
-                        title="Card Title 2"
-                        text="Some quick example text to build on the card title and make up the bulk of the card's content."
-                        imageClass="aspect-imagen"
-                        buttonText="Go somewhere"
-                        buttonHref="#"
-                    />
-                </div>
-                <div className="col-3">
-                    <FormCard
-                        imageSrc="./src/assets/dist/img/klem-logo.png"
-                        imageAlt="Klem Logo"
-                        title="Card Title 2"
-                        text="Some quick example text to build on the card title and make up the bulk of the card's content."
-                        imageClass="aspect-imagen"
-                        buttonText="Go somewhere"
-                        buttonHref="#"
-                    />
-                </div>
-                <div className="col-3">
-                    <FormCard
-                        imageSrc="./src/assets/dist/img/klem-logo.png"
-                        imageAlt="Klem Logo"
-                        title="Card Title 2"
-                        text="Some  the card title and make up the bulk of the card's content."
-                        imageClass="aspect-imagen"
-                        buttonText="Go somewhere"
-                        buttonHref="#"
-                    />
-                </div>
-                <div className="col-3">
-                    <FormCard
-                        imageSrc="./src/assets/dist/img/klem-logo.png"
-                        imageAlt="Klem Logo"
-                        title="Card Title 2"
-                        text="Some quick example text to build on the card title and make up the bulk of the card's content."
-                        imageClass="aspect-imagen"
-                        buttonText="Go somewhere"
-                        buttonHref="#"
-                    />
-                </div>
-            </div>
-        </section>
-    </>
-  );
+    return (
+        <>
+            <section className="forma-corp container">
+                <h1 className="text-center">Descubre todas las formaciones disponibles sobre tu empresa o mercado</h1>
+                <div className="row pb-5">
+                    <div className="col-12 pb-5">
+                        <FormCard
+                            imageSrc="./src/assets/dist/img/klem-logo.png"
+                            imageAlt="Klem Logo"
+                            title="Card Title 2"
+                            text="Some quick example text to build on the card title and make up the bulk of the card's content."
+                            imageClass="aspect-formacorp"
+                            buttonText="Go somewhere"
+                            buttonHref="/form-detail"
+                        />
+                    </div>
+                        <div className="col-3">
+                            <FormCard
+                                imageSrc="./src/assets/dist/img/klem-logo.png"
+                                imageAlt="Klem Logo"
+                                title="Card Title 1"
+                                text="Some quick example text to build on the card title and make up the bulk of the card's content."
+                                imageClass="aspect-imagen"
+                                buttonText="Go somewhere"
+                                buttonHref="#"
+                            />
+                        </div>
+                        <div className="col-3">
+                            <FormCard
+                                imageSrc="./src/assets/dist/img/klem-logo.png"
+                                imageAlt="Klem Logo"
+                                title="Card Title 2"
+                                text="Some quick example text to build on the card title and make up the bulk of the card's content."
+                                imageClass="aspect-imagen"
+                                buttonText="Go somewhere"
+                                buttonHref="#"
+                            />
+                        </div>
+                        <div className="col-3">
+                            <FormCard
+                                imageSrc="./src/assets/dist/img/klem-logo.png"
+                                imageAlt="Klem Logo"
+                                title="Card Title 2"
+                                text="Some  the card title and make up the bulk of the card's content."
+                                imageClass="aspect-imagen"
+                                buttonText="Go somewhere"
+                                buttonHref="#"
+                            />
+                        </div>
+                        <div className="col-3">
+                            <FormCard
+                                imageSrc="./src/assets/dist/img/klem-logo.png"
+                                imageAlt="Klem Logo"
+                                title="Card Title 2"
+                                text="Some quick example text to build on the card title and make up the bulk of the card's content."
+                                imageClass="aspect-imagen"
+                                buttonText="Go somewhere"
+                                buttonHref="#"
+                            />
+                        </div>
+                    </div>
+            </section>
+        </>
+    );
 };
 
 export default HomePage;
