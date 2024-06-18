3fc4c2417aee93586129fd5ef56472222906ae9c
paussaanz <paula@sanz.eu>
2024-06-18 18:27:05 +0200

    search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        <div>
+            <form className="form-inline pb-3">
+                <div className="input-group w-100">
+                    <input type="search" className="form-control" placeholder="Buscar" aria-label="Buscar" />
+                    <div className="input-group-append">
+                        <button className="btn btn-outline-success" type="submit">Buscar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default SearchBar;
