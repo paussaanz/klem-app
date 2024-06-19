bd4ba295f3c1de9fa1b7fd4773bd06153c69c1d8
paussaanz <paula@sanz.eu>
2024-06-19 21:11:15 +0200

    profile page fix

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from '../components/Button';
+import UserInfoBox from '../components/UserInfoBox';
 
 const ProfilePage = () => {
     return (
-        <div>
-            <div className="row pt-5 bg-light">
-                <div className="col-12 text-center">
-                    <img src="https://via.placeholder.com/150" className="img-fluid rounded-5" />
+        <>
+            <div className="row pt5 bg-light border shadow border-bottom border-end-0 border-start-0 border-top-0">
+                <div className="col-12 text-center py-4 ">
+                    <img src="https://via.placeholder.com/150" className="img-fluid rounded-circle" />
                     <h1 className="h3 pt-3">USERNAME</h1>
                     <Button
                     href="/"
@@ -14,7 +15,13 @@ const ProfilePage = () => {
                     text="Retar"/>
                 </div>
             </div>
-        </div>
+            <div className="row pt-4">
+                <div className="col-2">
+                    <UserInfoBox
+                    text="MIO Group"/>
+                </div>
+            </div>
+        </>
     );
 };
 
