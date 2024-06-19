a0d6a9d44d37c7331cc1e7d230551fd41e579bbd
paussaanz <paula@sanz.eu>
2024-06-19 21:09:52 +0200

    company card created

---
+++ src/components/CompanyCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DepartamentCard from './DepartamentCard';
+
+const CompanyCard = () => {
+    return (
+        <>
+            <h2 className="text-center">MIO ONE</h2>
+            <DepartamentCard
+            DepartamentName='Data BI'/>
+        </>
+    );
+};
+
+export default CompanyCard;
