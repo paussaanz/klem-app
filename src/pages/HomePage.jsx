b773f5b42667c43c8e71e7062735e492c2e0a196
paussaanz <paula@sanz.eu>
2024-06-19 21:11:03 +0200

    header fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,8 +6,12 @@ const HomePage = () => {
 
     return (
         <>
-            <section className="forma-corp container">
-                <h1 className="text-center">Descubre todas las formaciones disponibles sobre tu empresa o mercado</h1>
+            <section className="forma-corp container pt-4">
+                <div className="row">
+                    <div className="col-10 offset-1">
+                        <h1 className="text-center text-uppercase h2 text-primary">Descubre todas las formaciones disponibles sobre tu empresa o mercado</h1>
+                    </div>
+                </div>
                 <div className="row pb-5">
                     <div className="col-12 pb-5">
                         <FormCard
@@ -20,51 +24,51 @@ const HomePage = () => {
                             buttonHref="/form-detail"
                         />
                     </div>
-                        <div className="col-3">
-                            <FormCard
-                                imageSrc="./src/assets/dist/img/klem-logo.png"
-                                imageAlt="Klem Logo"
-                                title="Card Title 1"
-                                text="Some quick example text to build on the card title and make up the bulk of the card's content."
-                                imageClass="aspect-imagen"
-                                buttonText="Go somewhere"
-                                buttonHref="#"
-                            />
-                        </div>
-                        <div className="col-3">
-                            <FormCard
-                                imageSrc="./src/assets/dist/img/klem-logo.png"
-                                imageAlt="Klem Logo"
-                                title="Card Title 2"
-                                text="Some quick example text to build on the card title and make up the bulk of the card's content."
-                                imageClass="aspect-imagen"
-                                buttonText="Go somewhere"
-                                buttonHref="#"
-                            />
-                        </div>
-                        <div className="col-3">
-                            <FormCard
-                                imageSrc="./src/assets/dist/img/klem-logo.png"
-                                imageAlt="Klem Logo"
-                                title="Card Title 2"
-                                text="Some  the card title and make up the bulk of the card's content."
-                                imageClass="aspect-imagen"
-                                buttonText="Go somewhere"
-                                buttonHref="#"
-                            />
-                        </div>
-                        <div className="col-3">
-                            <FormCard
-                                imageSrc="./src/assets/dist/img/klem-logo.png"
-                                imageAlt="Klem Logo"
-                                title="Card Title 2"
-                                text="Some quick example text to build on the card title and make up the bulk of the card's content."
-                                imageClass="aspect-imagen"
-                                buttonText="Go somewhere"
-                                buttonHref="#"
-                            />
-                        </div>
+                    <div className="col-3">
+                        <FormCard
+                            imageSrc="./src/assets/dist/img/klem-logo.png"
+                            imageAlt="Klem Logo"
+                            title="Card Title 1"
+                            text="Some quick example text to build on the card title and make up the bulk of the card's content."
+                            imageClass="aspect-imagen"
+                            buttonText="Go somewhere"
+                            buttonHref="#"
+                        />
                     </div>
+                    <div className="col-3">
+                        <FormCard
+                            imageSrc="./src/assets/dist/img/klem-logo.png"
+                            imageAlt="Klem Logo"
+                            title="Card Title 2"
+                            text="Some quick example text to build on the card title and make up the bulk of the card's content."
+                            imageClass="aspect-imagen"
+                            buttonText="Go somewhere"
+                            buttonHref="#"
+                        />
+                    </div>
+                    <div className="col-3">
+                        <FormCard
+                            imageSrc="./src/assets/dist/img/klem-logo.png"
+                            imageAlt="Klem Logo"
+                            title="Card Title 2"
+                            text="Some  the card title and make up the bulk of the card's content."
+                            imageClass="aspect-imagen"
+                            buttonText="Go somewhere"
+                            buttonHref="#"
+                        />
+                    </div>
+                    <div className="col-3">
+                        <FormCard
+                            imageSrc="./src/assets/dist/img/klem-logo.png"
+                            imageAlt="Klem Logo"
+                            title="Card Title 2"
+                            text="Some quick example text to build on the card title and make up the bulk of the card's content."
+                            imageClass="aspect-imagen"
+                            buttonText="Go somewhere"
+                            buttonHref="#"
+                        />
+                    </div>
+                </div>
             </section>
         </>
     );
