535aad33d80dd30f42a83b429c5578c97bc6c1b7
paussaanz <paula@sanz.eu>
2024-06-19 21:10:15 +0200

    ranking list item bg white

--- src/components/RankingListItem.jsx
+++ src/components/RankingListItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RankingListItem = () => {
     return (
         <div className="px-2">
-            <div className="d-flex align-items-center border rounded py-2 px-3">
+            <div className="d-flex align-items-center border rounded py-2 px-3 bg-white cor-white">
                 <div className="flex-shrink-0">
                     <img src="https://via.placeholder.com/80" alt="Imagen" className="img-fluid  rounded-5" />
                 </div>
