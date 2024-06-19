6cb2097d6b94433d4f23d70a793e3e24bd6aa4ba
paussaanz <paula@sanz.eu>
2024-06-19 21:11:37 +0200

    talento page started

--- src/pages/TalentoPage.jsx
+++ src/pages/TalentoPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import CompanyCard from '../components/CompanyCard';
 
 const TalentoPage = () => {
     return (
-        <div>
-            <h1>TALENTO</h1>
-        </div>
+        <>
+            <div className="container pt-4">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="text-primary text-center text-uppercase h2">Encuentra todo el talento de tu empresa</h1>
+                    </div>
+                </div>
+                <div className="row justify-content-center bg-cream rounded pt-3">
+                    <CompanyCard/>
+                </div>
+            </div>
+        </>
     );
 };
 
